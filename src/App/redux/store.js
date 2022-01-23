c5c88f6bf9ef0b27125fca02e6bca992dc458f6f
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-24 03:03:16 +0530

    [Feat]: Initialzed redux dev tools
    - Added dev tools in store

--- src/App/redux/store.js
+++ src/App/redux/store.js
@@ -9,6 +9,9 @@ const rootReducer = combineReducers({
   styles: styleReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
