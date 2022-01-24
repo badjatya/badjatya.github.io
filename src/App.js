180b2083bca31a46ac271445d7b483fc5c009743
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-24 11:14:48 +0530

    [Feat]: Created a theme switcher
    - Created a Container component for checking the theme.
    - Updated the colors of light theme and dark theme
    - Added a dispatch function to change theme

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React from "react";
 
 // IMPORTING Redux and root reducer
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
+import { setDarkTheme, setLightTheme } from "./App/redux/actions/style.action";
 import store from "./App/redux/store";
 
 // Importing Styles
-import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./App/styles/globalStyles";
-import theme from "./App/styles/theme";
+import StyledThemeProvider from "./App/styles/theme";
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
+        <StyledThemeProvider>
           <GlobalStyle />
           <h1 style={{ color: "#fff", fontSize: "4rem" }} className="logo">
             B<span>adjatya</span>
           </h1>
-        </ThemeProvider>
+          <Container />
+        </StyledThemeProvider>
       </Provider>
     </>
   );
 }
+
+const Container = () => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <p>Hey</p>
+
+      <button onClick={() => dispatch(setDarkTheme())}>Dark Theme</button>
+      <br />
+      <button onClick={() => dispatch(setLightTheme())}>Light Theme</button>
+    </>
+  );
+};
 
 export default App;
--- src/App/styles/theme.js
+++ src/App/styles/theme.js
@@ -1,25 +1,15 @@
 // Importing Packages
-// import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 
-const theme = {
-  color: {
-    bg_900: "#ffffff",
-    bg_100: "#353535",
-    bg_50: "#2b2c2f",
-    bf_opacity: "rgba(255, 255, 255, 0.5)",
-    text_900: "#000000",
-    text_700: "#555555",
-    text_600: "#666666",
-    text_300: "#bbbbbb",
-    outer_shadow: "3px 3px 3px #d0d0d0, -3px -3px 3px #f8f8f8",
-    outer_shadow_0: "0 0 0 #d0d0d0, 0 0 0 #f8f8f8",
-    inner_shadow: "inset 3px 3px 3px #d0d0d0, inset -3px -3px 3px #f8f8f8",
-    inner_shadow_0: "inset 0 0 0 #d0d0d0, inset 0 0 0 #f8f8f8",
-  },
+// Redux
+import { useSelector } from "react-redux";
+
+const StyledThemeProvider = ({ children }) => {
+  const color = useSelector((state) => state.styles.theme.color);
+  const theme = {
+    color,
+  };
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
 
-// const StyledThemeProvider = ({ children }) => (
-//   <ThemeProvider theme={theme}>{children}</ThemeProvider>
-// );
-
-export default theme;
+export default StyledThemeProvider;
