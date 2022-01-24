3298d5bb77683ba4210136f0864bfc2d670b7dc4
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-24 17:52:32 +0530

    [Feat]: Created primary color data

--- src/App/data/theme.data.js
+++ src/App/data/theme.data.js
@@ -1,3 +1,4 @@
+// Default color data
 export const colorData = {
   bg_900: "#ffffff",
   bg_100: "#353535",
@@ -12,3 +13,27 @@ export const colorData = {
   inner_shadow: "inset 3px 3px 3px #222327, inset -3px -3px 3px #363636",
   inner_shadow_0: "inset 0 0 0 #222327, inset 0 0 0 #363636",
 };
+
+// Primary colors data
+export const primaryColorData = [
+  {
+    id: "7d8db85c-1054-49b3-83c4-f4a6d2b12804",
+    color: "#FB839E",
+  },
+  {
+    id: "8db29d4c-0b44-4eb6-bf33-39eade466c71",
+    color: " #EC9412",
+  },
+  {
+    id: "972b814e-e535-49d8-a899-b5d4c57ae91b",
+    color: "#1FC586",
+  },
+  {
+    id: "2e6b57cf-5ef4-4d41-89af-d9cf9a435db7",
+    color: "#2EB1ED",
+  },
+  {
+    id: "553b442f-4099-4fc2-bdad-70ccd46ef5ca",
+    color: "#CC3A3B",
+  },
+];
